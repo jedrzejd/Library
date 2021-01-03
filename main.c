a24f64573382aa7c0aed4513a81159c05fe0aedc
jedrzejd <jedrzejdudzicz@gmail.com>
2021-01-03 17:00:08 +0100

    testing files

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
-#include<book.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "book.h"
+#include "data.h"
+#include "customer.h"
+
 
 int main(){
-    
-    
+    FILE d = open_file("smiec.txt");
+    char name[20];
+
+    fscanf(&d, "%[^\n]", name);
+    close_file(d);    
+    printf("%s",name);
     return 0;
 }
