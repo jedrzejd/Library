67a16082c38b9238821e7484339c14f517c4540c
jedrzejd <jedrzejdudzicz@gmail.com>
2021-01-03 16:23:35 +0100

    Adding book.h with structure book

---
+++ book.h
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+struct book{
+    int dir_num;
+    char *author, *category, *date, *publ;
+    int isbn;
+    int availablity;
+};
+
+void Add_book(){
+    
+}
+
+void Edit_book(){
+
+}
+
+void Delete_book(){
+
+}
+
+void Print_Books(){
+    
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<book.h>
+
+int main(){
+    
+    
+    return 0;
+}
