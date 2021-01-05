8f93839415dcec64f1e9584547dbdcf92c3919e5
jedrzejd <jedrzejdudzicz@gmail.com>
2021-01-05 10:29:19 +0100

    Kurwa Odbugowane

--- book.h
+++ book.h
@@ -5,33 +5,42 @@ char plik[] = "books.txt";
 
 struct book{
     int dir_num;
-    char *author, *category;
+    char author[50], category[50];
     int date;
-    char *publ;
+    char publ[50];
     int isbn_code;
     int availablity;
 };
 
 void Add_book(struct book it){
-    FILE d = open_file(plik, "ra");
-    struct book temp;
+    FILE d = open_file(plik, "r");
+    struct book temp[10];
     int licz = 1;
     int ch = 0;
     while(1){
-        fscanf(&d, "%d %d %s %s %d %s %d %d\n", &ch, &temp.dir_num, temp.author, temp.category, &temp.date, temp.publ, &temp.isbn_code, &temp.availablity);
-        // if(ch == -1){
-        //     break;
-        // }
-        // printf("Line = %d\n", licz);
-        // printf("%d %s %s %d %s %d  %d\n", temp.dir_num, temp.author, temp.category, temp.date, temp.publ, temp.isbn_code, temp.availablity);        
-        // licz++;
-        break;
+        printf("Line = %d\n", licz);
+
+        fscanf(&d, "%d %d %s %s %d %s %d %d", &ch, &temp[licz].dir_num, temp[licz].author, temp[licz].category, &temp[licz].date, temp[licz].publ, &temp[licz].isbn_code, &temp[licz].availablity);
+        if(ch == -1){
+            printf("Koniec pliku!\n");
+            break;
+        }
+        // printf("%d %d %s %s %d %s %d %d\n", ch, temp[licz].dir_num, temp[licz].author, temp[licz].category, temp[licz].date, temp[licz].publ, temp[licz].isbn_code, temp[licz].availablity);        
+        licz++;
     }
+    close_file(d);
 
-    // printf("%d; %s; %s; %d; %s; %d; %d\n", it.dir_num, it.author, it.category, it.date, it.publ, it.isbn_code, it.availablity);
+    d = open_file(plik, "w");
 
+    for(int i=1; i<licz; i++){
+        // printf("%d %d %s %s %d %s %d %d\n", i, temp[i].dir_num, temp[i].author, temp[i].category, temp[i].date, temp[i].publ, temp[i].isbn_code, temp[i].availablity);        
+        fprintf(&d,"%d %d %s %s %d %s %d %d\n", i, temp[i].dir_num, temp[i].author, temp[i].category, temp[i].date, temp[i].publ, temp[i].isbn_code, temp[i].availablity);        
+    }
+    // printf("%d %d %s %s %d %s %d %d\n",licz,  it.dir_num, it.author, it.category, it.date, it.publ, it.isbn_code, it.availablity);
     fprintf(&d, "%d %d %s %s %d %s %d %d\n",licz,  it.dir_num, it.author, it.category, it.date, it.publ, it.isbn_code, it.availablity);
-
+    
+    // printf("-1 1 - - 1 - 1 1\n");
+    fprintf(&d, "-1 1 - - 1 - 1 1\n");
     close_file(d);    
 }
 
--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include "book.h"
-// #include "customer.h"
 
 
 int main(){
-    struct book ksiega= {1, "Arthura_Conana_Doyle", "murder_mystery", 1886, "Ward Lock_&_Co", 1123, 5};
+    struct book ksiega= {1, "Arthura_Conana_Doyle", "murder_mystery", 1886, "Ward_Lock_&_Co", 1123, 5};
     Add_book(ksiega);
     return 0;
 }
