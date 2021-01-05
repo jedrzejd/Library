ac5376381517b0b046c23c210ef1914c58f45f8a
jedrzejd <jedrzejdudzicz@gmail.com>
2021-01-05 11:22:18 +0100

    Adding cmakelists and adding new features

--- book.h
+++ book.h
@@ -1,7 +1,7 @@
 #include "data.h"
 #include <string.h>
 
-char plik[] = "books.txt";
+char plik[40] = "books.txt";
 
 struct book{
     int dir_num;
@@ -12,17 +12,25 @@ struct book{
     int availablity;
 };
 
+int comp(struct book a, struct book b){
+    printf("%s %s\n",a.author, b.author);
+    if((a.dir_num == b.dir_num )&&( a.date == b.date )&&( a.isbn_code == b.isbn_code )) {
+        return 1;
+    }
+    return 0;
+}
+
 void Add_book(struct book it){
     FILE d = open_file(plik, "r");
     struct book temp[10];
     int licz = 1;
     int ch = 0;
     while(1){
-        printf("Line = %d\n", licz);
+        // printf("Line = %d\n", licz);
 
         fscanf(&d, "%d %d %s %s %d %s %d %d", &ch, &temp[licz].dir_num, temp[licz].author, temp[licz].category, &temp[licz].date, temp[licz].publ, &temp[licz].isbn_code, &temp[licz].availablity);
         if(ch == -1){
-            printf("Koniec pliku!\n");
+            // printf("Koniec pliku!\n");
             break;
         }
         licz++;
@@ -44,12 +52,52 @@ void Edit_book(){
 
 }
 
-void Delete_book(){
+void Delete_book(struct book it){
+    FILE d = open_file(plik, "r");
+    struct book temp[10];
+    int licz = 1;
+    int ch = 0;
+    while(1){
+        // printf("Line = %d\n", licz);
 
+        fscanf(&d, "%d %d %s %s %d %s %d %d", &ch, &temp[licz].dir_num, temp[licz].author, temp[licz].category, &temp[licz].date, temp[licz].publ, &temp[licz].isbn_code, &temp[licz].availablity);
+        if(ch == -1){
+            // printf("Koniec pliku!\n");
+            break;
+        }
+        licz++;
+    }
+    close_file(d);
+
+    d = open_file(plik, "w");
+
+    for(int i=1; i<licz; i++){
+        if(comp(temp[i],it)==1){
+            fprintf(&d,"%d %d %s %s %d %s %d %d\n", i, temp[i].dir_num, temp[i].author, temp[i].category, temp[i].date, temp[i].publ, temp[i].isbn_code, temp[i].availablity);        
+        }
+    }
+    
+    fprintf(&d, "-1 1 - - 1 - 1 1\n");
+    close_file(d);    
 }
 
 void Print_Books(){
+    FILE d = open_file(plik, "r");
+    struct book temp[10];
+    int licz = 1;
+    int ch = 0;
+    while(1){
+        // printf("Line = %d\n", licz);
 
+        fscanf(&d, "%d %d %s %s %d %s %d %d", &ch, &temp[licz].dir_num, temp[licz].author, temp[licz].category, &temp[licz].date, temp[licz].publ, &temp[licz].isbn_code, &temp[licz].availablity);
+        if(ch == -1){
+            // printf("Koniec pliku!\n");
+            break;
+        }
+        printf("%d %d %s %s %d %s %d %d\n", licz, temp[licz].dir_num, temp[licz].author, temp[licz].category, temp[licz].date, temp[licz].publ, temp[licz].isbn_code, temp[licz].availablity);        
+        licz++;
+    }
+    close_file(d);
 }
 
 void Search_by_author(){
--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main(){
     struct book ksiega= {1, "Arthura_Conana_Doyle", "murder_mystery", 1886, "Ward_Lock_&_Co", 1123, 5};
+    struct book ksiega1= {1, "Wacław_Kubański", "murder_mystery", 1996, "Ward_Lock_&_Co", 11423, 2};
     Add_book(ksiega);
+    Add_book(ksiega1);
+    Delete_book(ksiega);
+    Print_Books();
     return 0;
 }
