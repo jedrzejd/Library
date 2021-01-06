bc3b571e3c97b47013d50075f15b5e8c6b32d605
jedrzejd <jedrzejdudzicz@gmail.com>
2021-01-06 10:27:29 +0100

    Adding new tests

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 
 int main(){
     struct book ksiega= {1, "Arthura_Conana_Doyle", "murder_mystery", 1886, "Ward_Lock_&_Co", 1123, 5};
-    struct book ksiega1= {1, "Wacław_Kubański", "murder_mystery", 1996, "Ward_Lock_&_Co", 11423, 2};
+    struct book ksiega1= {2, "Arthura_Conana_Doyle", "murder_mystery", 1887, "Ward_Lock_&_Co", 14123, 3};
+    struct book ksiega2= {3, "Arthura_Conana_Doyle", "murder_mystery", 1888, "Ward_Lock_&_Co", 1323, 1};
+    struct book ksiega3= {4, "Arthura_Conana_Doyle", "murder_mystery", 1889, "Ward_Lock_&_Co", 115523, 5};
+    struct book ksiega4= {5, "Wacław_Kubański", "murder_mystery", 1996, "Ward_Lock_&_Co", 11423, 2};
     Add_book(ksiega);
     Add_book(ksiega1);
-    Delete_book(ksiega);
-    Print_Books();
+    Add_book(ksiega2);
+    Add_book(ksiega3);
+    Add_book(ksiega4);
+//    Delete_book(ksiega4);
+//    Print_Books();
+    Search_by_author("Wacław_Kubański");
     return 0;
 }
